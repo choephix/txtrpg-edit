da565351ec2f17f431e9897f0921ce1ddacdcc54
Stefan <i@thechoephix.com>
2018-10-11 13:50:02 +0000

    ini from codio.com

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Game } from './game/game';
+
+declare var require: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css','./styling/main.css']
 })
 export class AppComponent {
-  title = 'txtrpg-poc-angular';
+  imgurl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa0ChPcUYH2D6lPGnxE-CcSI6hY0AXwg0HPQcRc1wf8cmzX0k8';
+
+  game:Game = new Game()
+
+  debug_log:string = ""
+
+  constructor()
+  {
+    this.game.start()
+
+//     this.debug_log = require('./data/mock-world.json')
+    console.log(require('./game/mock-world.json'))
+  }
 }
