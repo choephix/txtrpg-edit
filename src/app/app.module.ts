f419b9844ded8a48456c06e4fb634090efdbd328
Stefan <i@thechoephix.com>
2018-10-14 18:23:09 +0000

    added unimpl datagrid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
+import { AgGridModule } from 'ag-grid-angular';
 import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { AppComponent } from './app.component';
 import { GameViewComponent } from './game-view/game-view.component';
 import { EditorVewComponent } from './editor-vew/editor-vew.component';
 
@@ -20,6 +21,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    AgGridModule.withComponents([]),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     RouterModule.forRoot( appRoutes, { enableTracing: false } )
   ],
--- src/app/editor-vew/editor-vew.component.ts
+++ src/app/editor-vew/editor-vew.component.ts
@@ -5,7 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './editor-vew.component.html',
   styleUrls: ['./editor-vew.component.css']
 })
-export class EditorVewComponent implements OnInit {
+export class EditorVewComponent implements OnInit
+{
+  columnDefs = [
+      { editable:true, field: 'make' , headerName: 'Make' },
+      { editable:true, field: 'model', headerName: 'Model' },
+      { editable:true, field: 'price', headerName: 'Price' }
+  ];
+
+  rowData = [
+      { make: 'Toyota', model: 'Celica', price: 35000 },
+      { make: 'Ford', model: 'Mondeo', price: 32000 },
+      { make: 'Porsche', model: 'Boxter', price: 72000 }
+  ];
 
   constructor() { }
 
