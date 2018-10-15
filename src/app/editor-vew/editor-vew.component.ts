20218c68fe7fd37fe556abd048d5340ee550abd8
Stefan <i@thechoephix.com>
2018-10-15 02:54:15 +0000

    add and remove nodes at will

--- src/app/editor-vew/editor-vew.component.ts
+++ src/app/editor-vew/editor-vew.component.ts
@@ -121,30 +121,33 @@ export class EditorViewChild_NodeLinksTable
   #lesvg { height:99.5vh; width:100%; }
   .node { stroke:rgba(100,200,250,0.5); stroke-width:4; fill:rgba(50,100,200,1.0) }
   .node:hover { opacity : .9 }
-  .link { stroke:rgba(100,200,250,0.5); stroke-width:4 }
+  .link { stroke:rgba(100,200,250,0.25); stroke-width:16 }
   .label { font: bold 10px monospace, sans-serif; color: grey; user-select: none;  }
   `],
   template: `
   <svg id="lesvg" #lesvg
   			(mouseup)="mouseup($event)"
-  			(mousemove)="mousemove($event)">
+  			(mousemove)="mousemove($event)"
+  			(contextmenu)="contextmenu($event)">
   	<ng-container *ngFor="let link of w.node_links; let i = index">
-  		<line class="link"
+  		<line
   			[attr.data-index]="i"
   			[attr.x1]="getViewX(link.to)"
   			[attr.y1]="getViewY(link.to)"
   			[attr.x2]="getViewX(link.from)"
   			[attr.y2]="getViewY(link.from)"
-  			/>
+  			(mouseup)="mouseup_node($event)"
+  			class="link"/>
   	</ng-container>
   	<ng-container *ngFor="let node of w.nodes; let i = index">
-  		<circle class="node"
+  		<circle
   			[attr.data-index]="i"
   			[attr.cx]="getViewX(node)"
   			[attr.cy]="getViewY(node)"
   			[attr.r]="16"
-  			(mousedown)="mousedown($event)"
-  			/>
+  			(mousedown)="mousedown_node($event)"
+  			(mouseup)="mouseup_node($event)"
+  			class="node"/>
   		<text
   			[attr.x]="getViewX(node)"
   			[attr.y]="getViewY(node) + 24"
@@ -164,6 +167,7 @@ export class EditorViewChild_Map
   constructor()
   {
     this.w = require('./../game/mock-world.3.json');
+    // for ( let node of this.w.nodes ) node.loc_x -=500
   }
 
   getNodeByID( id )
@@ -195,7 +199,7 @@ export class EditorViewChild_Map
 
   mousemove(e)
   {
-  	if ( e.buttons > 0 )
+  	if ( e.buttons > 0 && e.button <= 1 )
   	{
 			if ( this.draggy )
 			{
@@ -214,18 +218,55 @@ export class EditorViewChild_Map
   	}
   }
 
-  mousedown(e)
+  mousedown_node(e)
   {
   	let node_id = +e.target.attributes['data-index'].value
   	this.draggy = this.w.nodes[node_id];
+  }
+
+  mouseup_node(e)
+  {
+  	let node_index = +e.target.attributes['data-index'].value
+  	let node = this.w.nodes[node_index]
+
+  	if ( e.button == 1 )
+  	{
+  		this.w.nodes.splice(node_index, 1)
+  		let links = this.w.node_links
+  		for ( let i = links.length - 1; i >= 0; i-- )
+  			if ( links[i].to == node.id || links[i].from == node.id )
+  				this.w.node_links.splice( i, 1 )
+  	}
+  	else
+  	if ( e.button == 2 )
+  	{
+  		let new_id = `node_${this.w.nodes.length}`
+  		let title = new_id
+  		this.w.nodes.push({
+  			id:new_id,
+  			title:title,
+  			loc_x:node.loc_x + Math.random() * 64,
+  			loc_y:node.loc_y - Math.random() * 64,
+  		})
+  		this.w.node_links.push({from:new_id,to:node.id,handle_goto:`Go to ${title}`,handle_gobackto:``})
+  		this.w.node_links.push({from:node.id,to:new_id,handle_goto:`Go to ${node.title}`,handle_gobackto:``})
+  	}
+
+  	// console.log(e)
+  	// console.log(this.draggy)
   	// this.log(node_id)
   }
 
   mouseup(e)
   {
   	this.draggy = null;
-  	// console.log(JSON.stringify(this.w))
+	  // console.log(e)
+
+  	if ( e.button == 1 )
+  	  console.log(JSON.stringify(this.w))
   }
+
+  contextmenu(e) { return false; }
 
   log(o) { console.log(o) }
 }
