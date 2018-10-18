46ed7b004ee998123747f710177063e4b484ba1d
Stefan <i@thechoephix.com>
2018-10-18 01:19:53 +0000

    moved 404

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,10 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-	public ALL_BRANCHES:string[] = ["master","develop","poc","lorem"]
-	public branch:string = "develop"
+  public ALL_BRANCHES:string[] = ["master","develop","poc","lorem"]
+  public branch:string = "develop"
   constructor( public router:Router )
-  { router.events.subscribe( e => this.branch = router.url.match(/^\/?([^\/]*)\//)[1] ) }
-}
-
-@Component({
-	styles: [`
-	body,div,p { overflow:hidden; }
-	#fof {
-		font-size:56vw;
-		font-family:"Arial Black";
-		text-align:center;
-    margin-left: -100%;
-    margin-right: -100%;
-		opacity:.1;
-    transform: rotate(15deg);
-	}`],
-  template: `<div id='fof'>404</div>`,
-})
-export class Page404Component {
-  constructor() {}
+  { 
+      router.events.subscribe( e => this.branch = router.url.match(/^\/?([^\/]*)\//)[1] ) 
+  }
 }
