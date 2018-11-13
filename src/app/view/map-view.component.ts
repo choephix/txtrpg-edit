3df0c970885af8dcd71eef65d2ec653711e7b6bc
choephix <dev@thechoephix.com>
2018-11-13 06:07:33 +0200

    css fixes

--- src/app/view/map-view.component.ts
+++ src/app/view/map-view.component.ts
@@ -1,4 +1,21 @@
 import { Component } from '@angular/core';
 
-@Component({ template: `<map-pane></map-pane>` })
+@Component({template: `
+<div id="full-page" class="darkform">
+  <div class="pane left" style="width: 61.8%;">
+    <map-pane></map-pane>
+  </div>
+  <!-- SPLIT -->
+  <div class="pane right" style="width: 38.2%;">
+    <goto-actions-pane></goto-actions-pane>
+  </div>
+</div>`,
+styles: [
+require("../variables.scss"),
+require("../darkform.scss"), `
+#full-page { display: flex; flex-direction: row; height:100vh; overflow:hidden; }
+.pane { overflow-x: visible; overflow-y: scroll; min-height: 100%; }
+.pane.left { overflow-y: auto; }
+.pane.right { border-left: #222 1px solid; }
+`]})
 export class EditorViewChild_Map {}
