a12c9824b82edbbddc717e088957e2bc0ae0bf44
choephix <dev@thechoephix.com>
2018-11-17 06:09:59 +0200

    ace

--- src/app/common/json-ace-editor.component.ts
+++ src/app/common/json-ace-editor.component.ts
@@ -2,16 +2,37 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'json-ace-editor',
-  template: `<ace-editor mode="json" [theme]="theme" [options]="options" [(text)]="json"></ace-editor>`
+  styles:[`
+  ace-editor {
+    transition-property: opacity, background-color;
+    transition-duration: 300ms;
+  }
+  ace-editor.error {
+    background: #311 !important;
+  }
+  `],
+  template: `
+  <ace-editor
+    mode="json"
+    [theme]="theme"
+    [options]="options"
+    [(text)]="json"
+    [class.error]="error"
+    [style.minHeight]="minHeight"
+    ></ace-editor>`
 })
 export class JsonAceEditorWrapperComponent
 {
-  @Input() theme:string = "tomorrow_night_eighties"
+  @Input() theme:string = "monokai"
   @Input() data:object = {}
 
 	@Input() fontSize:string = `12px`
-	@Input() showGutter:boolean = false
-	@Input() showLineNumbers:boolean = false
+	@Input() showGutter:boolean = true
+  @Input() showLineNumbers:boolean = true
+  @Input() minHeight:string = "25vh"
+  // @Input() minLines:number = 32
+
+  error:Error = null
 
   get options():object { return {
       fontSize: this.fontSize,
@@ -21,14 +42,29 @@ export class JsonAceEditorWrapperComponent
       fadeFoldWidgets: true,
       showPrintMargin: false,
       wrap: true,
+      tabSize: 2,
+      useSoftTabs: true,
       scrollPastEnd: 0.5,
       maxLines: Infinity,
-      minLines: 30,
-      tabSize: 2,
-      useSoftTabs: true
+      // minLines: this.minLines,
     }
   }
 
-  get json():string { return JSON.stringify(this.data,null,2)+"\n\n\n\n" }
-  set json(value:string) { if(value&&value!="null") Object.assign(this.data,JSON.parse(value)) }
+  get json():string {
+    return JSON.stringify(this.data,null,2)+"\n\n\n\n"
+  }
+  set json(value:string)
+  {
+    if( !value )
+      return
+    try
+    {
+      this.error = null
+      Object.assign(this.data,JSON.parse(value))
+    }
+    catch ( error )
+    {
+      this.error = error
+    }
+  }
 }
